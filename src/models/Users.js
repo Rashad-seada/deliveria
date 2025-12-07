67ff1be01d7609d41c5931fa454a6a42e3eb3a03
EbrahimShoeib <ibrahimshoeib255@gmail.com>
2025-12-07 15:08:03 +0200

    fix : Auth controller

--- src/models/Users.js
+++ src/models/Users.js
@@ -10,7 +10,11 @@ const userSchema = new Schema({
   password: { type: String, required: true },
   ban: { type: Boolean, default: false },
   fcm_token: { type: String, default: null },
-  points: { type: Number, default: 0 }
+  points: { type: Number, default: 0 },
+  address_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Address"
+  }
 }, { timestamps: true });
 
 userSchema.statics.isPhoneTaken = async function(phone) {
